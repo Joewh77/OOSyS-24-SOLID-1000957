fc7a95915d16cb0f963ca702f4774e149ba4e4fc
Joewh77 <159479720+Joewh77@users.noreply.github.com>
2024-04-17 12:45:32 +0100

    Update SupplyHub.java
    JavaDoc

--- OOSyS_VETS/src/ljmu/vets/SupplyHub.java
+++ OOSyS_VETS/src/ljmu/vets/SupplyHub.java
@@ -1,5 +1,11 @@
 package ljmu.vets;
 
+/** Represents the supply hub.
+ * @author Joe Whittaker
+ * @version 4.26.0
+ * @since 1.0
+*/
+
 public class SupplyHub implements Notifiable {
 	private String name;
 
