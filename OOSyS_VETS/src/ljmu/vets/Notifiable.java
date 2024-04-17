b0e5392f0c451b87e23074493a775dd2af10a502
Joewh77 <159479720+Joewh77@users.noreply.github.com>
2024-04-17 12:40:27 +0100

    Update Notifiable.java
    JavaDoc

--- OOSyS_VETS/src/ljmu/vets/Notifiable.java
+++ OOSyS_VETS/src/ljmu/vets/Notifiable.java
@@ -1,5 +1,11 @@
 package ljmu.vets;
 
+/** Represents notifiable option.
+ * @author Joe Whittaker
+ * @version 4.26.0
+ * @since 1.0
+*/
+
 public interface Notifiable {
 	public void notifyy(String s);
 }
