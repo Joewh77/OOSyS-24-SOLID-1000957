3e735104829844735fc7a50ff3fa848edcddfdac
Joewh77 <159479720+Joewh77@users.noreply.github.com>
2024-04-17 12:42:39 +0100

    Update Payable.java
    JavaDoc

--- OOSyS_VETS/src/ljmu/vets/Payable.java
+++ OOSyS_VETS/src/ljmu/vets/Payable.java
@@ -1,5 +1,11 @@
 package ljmu.vets;
 
+/** Represents payable which is used to get surgery costs.
+ * @author Joe Whittaker
+ * @version 4.26.0
+ * @since 1.0
+*/
+
 public interface Payable {
 	public Double getSurgeryCost();
 	public Double getPublicCost() throws Exception;
