93eb60852a0ef97e33504265f64ebaba1505352b
Joewh77 <159479720+Joewh77@users.noreply.github.com>
2024-04-17 16:48:49 +0100

    Update Invoice.java
    Removing SavedAs functions from invoice class

--- OOSyS_VETS/src/ljmu/vets/Invoice.java
+++ OOSyS_VETS/src/ljmu/vets/Invoice.java
@@ -44,38 +44,3 @@ public class Invoice implements Serializable {
 
 		return Double.parseDouble(new DecimalFormat("#.##").format(tt));
 	}
-
-	public void saveAs(FileType fileType, String path) {
-		switch (fileType) {
-			case DOCX : {
-				saveAsDocX(path);
-				break;
-			}
-			case XPS : {
-				saveAsXPS(path);
-				break;
-			}
-			case PDF : {
-				saveAsPDF(path);
-				break;
-			}
-		}
-	}
-	
-	// ToDo : Private !
-	private void saveAsDocX(String path) {
-		
-	}
-
-	// ToDo : Private !
-	private void saveAsXPS(String path) {
-		
-	}
-
-	// ToDo : Private !
-	private void saveAsPDF(String path) {
-		
-	}
-
-	// ToDo : get / set Methods ?
-}
