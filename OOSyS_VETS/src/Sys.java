7972ef94c62519523c9d78f3580aa4f7851c65e7
Joewh77 <159479720+Joewh77@users.noreply.github.com>
2024-04-18 17:27:53 +0100

    Update Sys.java
    Addition of SavedSampleData to Sys class.
    Altered given sample data to include JUnit 5's assertThrows.

--- OOSyS_VETS/src/Sys.java
+++ OOSyS_VETS/src/Sys.java
@@ -24,14 +24,15 @@ public class Sys {
 
 	private List<Surgery> surgeries = new ArrayList<Surgery>();
 	private Surgery surgery;
+	private SavedSampleData ssd;
 
-	// ToDo : Necessary ?
-	public Sys() {
+	// Attempt to add any sample data given to another class (SavedSampleData) to be saved.
+	public Sys(SavedSampleData ssd) throws Exception {
 		deSerialize();
 
 		/* NOTE : Debugging !
-		surgeries.add(new Surgery("SurgeryA"));
-		surgeries.add(new Surgery("SurgeryB"));
+		surgeries.add(new Surgery("SurgeryA", DayOfWeek.THURSDAY));
+		surgeries.add(new Surgery("SurgeryB", DayOfWeek.FRIDAY));
 
 		Pet pet1 = new Cat("GlynH", LocalDate.of(2020, 11, 11), Breeding.MOGGIE);
 		surgeries.get(0).makePet(pet1);
