384d649a240dec3a79b19a7033169e017075522c
Joewh77 <159479720+Joewh77@users.noreply.github.com>
2024-04-19 15:52:45 +0100

    Update Sys.java
    Commenting out code that doesn't work.

--- OOSyS_VETS/src/Sys.java
+++ OOSyS_VETS/src/Sys.java
@@ -34,6 +34,7 @@ public class Sys {
 		/* NOTE : Debugging !
 		surgeries.add(new Surgery("SurgeryA", DayOfWeek.THURSDAY));
 		surgeries.add(new Surgery("SurgeryB", DayOfWeek.FRIDAY));
+		*/
 
 		Pet pet1 = new Cat("GlynH", LocalDate.of(2020, 11, 11), Breeding.MOGGIE);
 		surgeries.get(0).makePet(pet1);
@@ -46,7 +47,7 @@ public class Sys {
 
 		Booking booking3 = new Booking("SurgeryA-REF3", pet1, LocalDateTime.of(2025, 11, 11, 11, 00), 90);
 		surgeries.get(0).makeBooking(booking3);
-		*/
+		
 	}
 
 	public void entryMenu() {
@@ -194,7 +195,7 @@ public class Sys {
 		System.out.print("Booking's Duration [i.e. MINS] : ");
 		Integer duration = Integer.parseInt(S.nextLine());
 
-		surgery.makeBooking(new Booking(ref, pet, when, duration));
+		//surgery.makeBooking(new Booking(ref, pet, when, duration));
 	}
 
 	private void getPetsNextBooking() {
